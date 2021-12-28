82244e24027d25346e0369ed2d0f9397a7bf1fbe
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-28 18:03:07 +0530

    Update stringify.ts

--- packages/core/src/stringify.ts
+++ packages/core/src/stringify.ts
@@ -7,24 +7,18 @@ import { CSSAttribute, Style } from './types';
  * @param {String} wrapper
  */
 export let stringify = (obj: CSSAttribute, selector = '{-}'): Style[] => {
-    let blocks: {
-        prop: string;
-        val: string;
-        i: boolean;
-        sel: string;
-        at: string[];
-    }[] = [];
+    let blocks: Style[] = [];
 
     let decl = (key: string, val: string | number | undefined) => {
         // Push the line for this property
         if (val != undefined) {
-            blocks.push({
-                prop: (key = key.replace(/[A-Z]/g, '-$&').toLowerCase()),
-                val: val + '',
-                i: false,
-                sel: selector,
-                at: []
-            });
+            blocks.push([
+                (key = key.replace(/[A-Z]/g, '-$&').toLowerCase()),
+                val + '',
+                false,
+                selector,
+                []
+            ]);
         }
     };
 
@@ -35,7 +29,7 @@ export let stringify = (obj: CSSAttribute, selector = '{-}'): Style[] => {
             // If these are the `@` rule
             if (key[0] == '@')
                 stringify(val as CSSAttribute, selector).forEach((block) => {
-                    block.at.push(key);
+                    block[4].push(key);
                     blocks.push(block);
                 });
             else if (Array.isArray(val)) val.forEach((val) => decl(key, val));
