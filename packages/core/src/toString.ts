15952d4df1235eb1f6d7e5aa6da6b7e5f98667c2
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-28 18:16:10 +0530

    Delete toString.ts

--- packages/core/src/toString.ts
+++
@@ -1,9 +0,0 @@
-import { Style } from './types';
-
-export let toString = ([prop, val, i, sel, at]: Style) => {
-    at.unshift(sel);
-    return at.reduce(
-        (acc, atRule) => `${atRule}{${acc}}`,
-        prop + ':' + val + (i ? ' !important' : '') + ';'
-    );
-};
