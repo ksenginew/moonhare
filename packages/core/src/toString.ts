1b42652d452d24bf6aa02c6c99cfe4d40b537b90
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-28 18:00:49 +0530

    Update toString.ts

--- packages/core/src/toString.ts
+++ packages/core/src/toString.ts
@@ -1,6 +1,6 @@
 import { Style } from './types';
 
-export let toString = ({ prop, val, i, sel, at }: Style) => {
+export let toString = ([prop, val, i, sel, at]: Style) => {
     at.unshift(sel);
     return at.reduce(
         (acc, atRule) => `${atRule}{${acc}}`,
