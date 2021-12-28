2d519ac0e39d0dadd1f9d0bf3cc6e85cac4f72a3
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-28 18:19:56 +0530

    Update index.ts

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -1,28 +1,13 @@
 import type { Options, Style } from '@moonhare/core';
-
-let id = 'mh'
-
-/**
- * Returns the _commit_ target
- */
-export let getSheet = (target: HTMLElement = document.head) => {
-    return (target.querySelector('#' + id) as HTMLStyleElement
-        || target.appendChild(
-            Object.assign(document.createElement('style'), {
-                id,
-                innerHTML: ' '
-            })
-        )
-    ).sheet as CSSStyleSheet
-};
+import {stringify } from './stringify'
+import {getSheet} from './stringify'
 
 interface DOMOptions extends Options {
     container: HTMLElement;
 }
 
 export let render = (options: Partial<DOMOptions> = {}) => {
-    let add = (style: Style, index: number) => getSheet().insertRule((options as DOMOptions).toString(style), index);
-
+    let add = (style: Style, index: number) => getSheet().insertRule(stringify(style), index);
 
     let remove = (index: number) => getSheet().deleteRule(index);
 
