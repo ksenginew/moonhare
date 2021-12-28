25962f34c692868a71a735c8dae9ac1d8c042d53
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-28 18:17:30 +0530

    Create stringify.ts

---
+++ packages/dom/src/stringify.ts
@@ -0,0 +1,8 @@
+import type {Style} from '@moonhare/core'
+export let toString = ([prop, val, i, sel, at]: Style) => {
+    at.unshift(sel);
+    return at.reduce(
+        (acc, atRule) => `${atRule}{${acc}}`,
+        prop + ':' + val + (i ? ' !important' : '') + ';'
+    );
+};
