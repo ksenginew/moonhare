e3cefb77a9a6cab0870be26510e10ae126af47e7
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-13 20:49:52 +0530

    commit

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,40 +1,70 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import styles from './index.module.css'
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext()
+    return (
+        <header className={clsx('hero  hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.tagline}</h1>
+                <p className="hero__subtitle">test</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg button--block"
+                        to="/docs/intro"
+                    >
+                        Tutorial
+                    </Link>
+                </div>
+            </div>
+        </header>
+    )
+}
+function HomepageContent() {
+    const { siteConfig } = useDocusaurusContext()
+    return (
+        <>
+            <header className={clsx('hero', styles.heroBanner)}>
+                <div className="container">
+                    <h2>Yet another CSS framework! Why?</h2>
+                    <p>
+                        When I heard about atomic CSS I loved that concept. Here I am entering the
+                        competition with an on demand lightweight atomic CSS engine with unlimited
+                        theme.
+                    </p>
+                    <div className="avatar avatar--vertical">
+                        <a
+                            className="avatar__photo-link avatar__photo avatar__photo--lg"
+                            href="https://twitter.com/ksenginew"
+                        >
+                            <img
+                                alt="Kavindu Santhusa Twitter Profile"
+                                src="https://github.com/ksenginew.png?size=128"
+                            />
+                        </a>
+                        <div className="avatar__intro">
+                            <div className="avatar__name">Kavindu Santhusa</div>
+                            <small className="avatar__subtitle">Creator of MoonHare CSS</small>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    const { siteConfig } = useDocusaurusContext()
+    return (
+        <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
+            <HomepageHeader />
+            <main>
+                <HomepageContent />
+            </main>
+        </Layout>
+    )
 }
